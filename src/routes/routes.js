ff3b9a1d06232d70ec1327429b7b90c6104c9ac1
Abdulhafiz Ahmed <abdulhafizahmed01@gmail.com>
2019-10-09 20:39:12 +0700

    #169036299 redirect link to actual url

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,14 @@
 import { renderLandingPage, trimUrl, deleteUrl } from "../middlewares/middlewares";
+import {Router} from 'express'
+import linkController from '../controllers/link';
+const router = Router()
 
 export default initRoutes = (app)=> {
 	app.get('/', (req, res)=> renderLandingPage(req, res));
 
 	app.post('/api/clip', (req, res)=> trimUrl(req, res));
+
+	router.route('/api/clip/:id', linkController.redirectLink)
 
 	app.delete('/api/clip/:id', (req, res)=> deleteUrl(req, res));
 };
