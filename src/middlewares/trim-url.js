1b8e7851e1b1b2b11632d04f6644718d965550f4
Akinlade Feranmi <feranmiakinlade@gmail.com>
2019-10-09 16:30:18 +0100

    Done: Creating and storing urlCode in trim-url.js

--- src/middlewares/trim-url.js
+++ src/middlewares/trim-url.js
@@ -1,33 +1,71 @@
 import UrlShorten from '../models/UrlShorten.js';
+import DeletedClip from '../models/deleted-clips';
 import { domainName } from '../config/constants';
 //import isEmpty from 'is-empty';
 //import crypto from 'crypto';
 
 export const trimUrl = async (req, res) => {
 	//First, check for orphaned urlCodes in the deleted-clips collection. If found, reassign the oldest one.
+	DeletedClip.find()
+		.sort({deletedAt: 'ascending'}) //In ascending order, document[0] should be the oldest. correct me if I'm wrong pls.
+		.then((deletedClips)=> {
+			if (deletedClips.length < 1) return; //If there are currently no deleted clips, go on to generate a new one.
+
+			const newClip = new UrlShorten({ //Reassign the oldest deleted clip to the new long url.
+				long_url: req.body.long_url,
+				clipped_url: deletedClips[0].clipped_url,
+				urlCode: deletedClips[0].urlCode,
+				created_by: req.cookies.userId,
+				click_count: 0
+			});				
+
+			newClip.save((err, savedClip)=> {
+				if (err) 
+					return res.status(500).json({
+						error: err
+					});
+				
+				DeletedClip.deleteOne({...deletedClips[0]}).exec(); //Once a deleted clip is reassigned, remove it from the DeletedClips collection.
+				res.status(201).json(savedClip);
+			});
+		})
+		.catch((error)=> {
+			res.status(500).json({
+				error: error
+			});
+		});
 
 	//If there are no orphaned urlCodes, generate a new one.
 		//Probably better to use UrlShorten.estimatedDocumentCount(); cos it's is faster. But does it always give the exact count? (cos it says "estimated").
-	const count = await UrlShorten.countDocuments({});
-	const newClipCount = count + 1;
+	UrlShorten.countDocuments({}, (error, count)=> {
+		if (error) 
+			return res.status(500).json({
+				error: error
+			});
 
-	//Create an alpha-numeric string representation of the count by converting it to base 36. (10 digits + 26 letters)
-	const newUrlCode = newClipCount.toString(36); //36 is the highest supported radix. 
-	if (newUrlCode.length < 4)
-		newUrlCode = '00' + newUrlCode; //Pad Url codes less that 4 characters on the left.
+		const newClipCount = count + 1;
 
-	const newClip = new UrlShorten({
-		long_url: req.body.long_url,
-		clipped_url: domainName + '/' + newUrlCode,
-		urlCode: newUrlCode,
-		created_by: req.cookies.userId,
-		click_count: 0
-	});
+		//Create an alpha-numeric string representation of the count by converting it to base 36. (10 digits + 26 letters)
+		const newUrlCode = newClipCount.toString(36); //36 is the highest supported radix. 
+		if (newUrlCode.length < 4)
+			newUrlCode = '00' + newUrlCode; //Pad Url codes less that 4 characters with zeros.
 
-	newClip.save((err)=> {
-  	if (err) return handleError(err);
-		
-		res.send(JSON.stringify(newClip));
+		const newClip = new UrlShorten({
+			long_url: req.body.long_url,
+			clipped_url: `${domainName}/${newUrlCode}`,
+			urlCode: newUrlCode,
+			created_by: req.cookies.userId,
+			click_count: 0
+		});
+
+		newClip.save((err, savedClip)=> {
+			if (err) 
+				return res.status(500).json({
+					error: error
+				});
+			
+			res.status(201).json(savedClip);
+		});
 	});
 }
   
