eaa4411a663fe0062d55c5aac6949efeacd017aa
Busola <olowubusolaolutayo1992@gmail.com>
2019-10-09 08:19:31 +0100

    move database variable to config file

--- src/database/db.js
+++ src/database/db.js
@@ -1,7 +1,5 @@
 let mongoose = require('mongoose');
-require('dotenv').config();
-
-const server = process.env.DATABASEURL;
+let {DATABASEURL}=require('../config/constant')
 class Database {
   constructor() {
     this._connect();
@@ -9,7 +7,7 @@ class Database {
 
   _connect() {
     mongoose
-      .connect(server, { useNewUrlParser: true, useUnifiedTopology: true })
+      .connect(DATABASEURL, { useNewUrlParser: true, useUnifiedTopology: true })
       .then(() => {
         console.log('Database connected');
       })
