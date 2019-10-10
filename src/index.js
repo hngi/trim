21886a8accfb2acc7fa8d29d079bd5aabffe1e2a
Busola <olowubusolaolutayo1992@gmail.com>
2019-10-10 22:52:50 +0100

    cleanup code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const { PORT } = require('./config/constants');
+const { PORT,SECRET_KEY } = require('./config/constants');
 const { initRoutes } = require('./routes/routes');
 const db = require('./database/db');
 
@@ -10,24 +10,32 @@ const app = express();
 
 app.use((req, res, next) => {
   //res.setHeader('Access-Control-Allow-Origin', '*'); //Don't think we need CORS here.
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
   next();
 });
 
 // load local css and js files
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser('super-secret-secret')); //Parse the cookie data (User ID).
-app.use(session({
-    secret: env.SECRET_KEY,
-    resave: true,
-}));
+app.use(
+  session({
+    secret: SECRET_KEY,
+    resave: true
+  })
+);
 
 app.set('view engine', 'ejs');
 
 initRoutes(app);
 
-app.listen(PORT, ()=> console.log(`Server listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
--- src/middlewares/validateCookie.js
+++ src/middlewares/validateCookie.js
@@ -1,5 +1,5 @@
 export const validateCookie = (req, res, next) => {
-  const userID = req.cookies.userId;
+  let userID = req.cookies.userId;
   if (userID == undefined) {
     userID = req.session.id;
     res.cookie("userID", userID, { maxAge: 90000, httpOnly: true });
