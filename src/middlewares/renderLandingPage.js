2b2504d5a07b34f7d0ee8fce4ae804865dc67fd0
Philzace1 <unknownumeh@gmail.com>
2019-10-10 01:41:39 +0100

    Task8-clippedLinks-should be sorted

--- src/middlewares/renderLandingPage.js
+++ src/middlewares/renderLandingPage.js
@@ -4,7 +4,7 @@ export const renderLandingPage = (req, res) => {
 	UrlShorten.find({
     created_by: req.cookies.userID //Find all clips created by this user.
 	}).sort({
-		createdAt: 'desc'
+		createdAt: 'desc' // sort the found clips by desention 
 	})
 	.then((clips) => { //Pass the user's clips to the view engine to render the customized view for this user.
 		res.render('../views/index', {userClips: clips});
