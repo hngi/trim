a6de1f29641fcaff57c9ab6f7b5265e2cdae11cc
Philzace1 <unknownumeh@gmail.com>
2019-10-10 12:04:48 +0100

    sorting clips

--- src/middlewares/renderLandingPage.js
+++ src/middlewares/renderLandingPage.js
@@ -11,7 +11,7 @@ export const renderLandingPage = (req, res) => {
   UrlShorten.find({
     created_by: req.cookies.userID //Find all clips created by this user.
 	}).sort({
-		createdAt: 'desc' // sort the found clips by desention 
+		createdAt: 'desc' // sort the clips decending 
 	})
 	.then((clips) => { //Pass the user's clips to the view engine to render the customized view for this user.
 		res.render('../src/views/index', {userClips: clips});
