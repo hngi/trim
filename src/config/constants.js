df8a3a4b7feaa6524bafeb5ec6f997bee43f8026
Akinlade Feranmi <feranmiakinlade@gmail.com>
2019-10-10 12:01:12 +0100

    Final commit. Battery low.

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,11 +2,11 @@ import dotenv from 'dotenv';
 
 const parsedEnv = dotenv.config();
 
-const { PORT, DATABASEURL } = parsedEnv.parsed;
+const { PORT, DB_URL } = parsedEnv.parsed;
 //console.log(DATABASEURL);
 
 module.exports = {
   PORT: process.env.PORT || PORT,
-  DB_URL: process.env.DB_URL || DATABASEURL,
-  domainName: 'trim.herokuapp.com'
+  DB_URL: process.env.DB_URL || DB_URL,
+  domainName: process.env.HOSTNAME
 }
--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Methods',
     'GET, POST, PUT, DELETE, PATCH, OPTIONS'
   );
-  console.log('Response headers set');
+  //console.log('Response headers set');
   next();
 });
 
