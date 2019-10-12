91db077a1438499e834cf3fa521079324b3f5aa4
unknown <fegoworks@hotmail.com>
2019-10-13 00:26:40 +0100

    feat(test): write test for GET by ID  endpoint

--- src/tests/index.js
+++ src/tests/index.js
@@ -1,9 +1,17 @@
 import chaiHttp from 'chai-http';
 import chai from 'chai';
 import app from '../index';
+import UrlShorten from "../models/UrlShorten";
+
+
+
 
 chai.use(chaiHttp);
-const { expect } = chai;
+chai.should()
+
+const {
+  expect
+} = chai;
 
 describe('Home page', () => {
   it('it should take users to the landing page', (done) => {
@@ -15,3 +23,40 @@ describe('Home page', () => {
       });
   });
 });
+
+
+describe('URL', () => {
+  beforeEach((done) => { //Before each test we empty the database
+    UrlShorten.remove({}, (err) => {
+      done();
+    });
+  });
+  /*
+   * Test the /GET route
+   */
+  describe('/GET Url', () => {
+    it('it should GET all the Urls', (done) => {
+      chai.request(app)
+        .get('/')
+        .end((err, res) => {
+          expect(res).to.have.status(200)
+          expect(res).to.be.a('object')
+          done();
+        });
+    });
+  });
+
+  describe('/GET/:id newTrim', () => {
+    it('it should give an error', (done) => {
+      const id = "1";
+      chai.request(app)
+        .get(`/${id}`)
+        .end((err, res) => {
+          expect(res).to.have.status(404)
+          done();
+        })
+    })
+  })
+
+
+})
