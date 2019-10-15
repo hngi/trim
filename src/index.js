ca7344a2cc37a4efc2fc728c8a8e3921b7ab59d4
Abdulhafiz Ahmed <abdulhafizahmed01@gmail.com>
2019-10-15 17:15:38 +0700

    moved public directory inside the src folder

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 });
 
 app.set('views', path.join(__dirname, 'views')) // Redirect to the views directory inside the src directory
-app.use(express.static(path.join(__dirname, '../public'))); // load local css and js files
+app.use(express.static(path.join(__dirname, 'public'))); // load local css and js files
 app.set('view engine', 'ejs'); 
 
 app.use(express.json());
