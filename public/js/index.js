e7cfb8ea328088fd3316c662c7710daa11bc932a
Abdulhafiz Ahmed <abdulhafizahmed01@gmail.com>
2019-10-16 15:09:50 +0700

    [#169131513] Add expiring trim frontend

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,85 @@
-trimUrlForm = document.querySelector('.trim-url-form');
+const trimUrlForm = document.querySelector('.trim-url-form');
+const tr_body = document.querySelector('#tbody')
+
+let tr_clip = document.createElement('tr')
+let td_click_count = document.createElement('td')
+let td_long_url = document.createElement('td')
+let td_trimmed = document.createElement('td')
+let td_expiry = document.createElement('td')
+let td_action_btn = document.createElement('td')
+let a_trimmed = document.createElement('a')
+let a_action_btn = document.createElement('a')
+let a_whatsapp = document.createElement('a')
+let a_facebook = document.createElement('a')
+let a_twitter = document.createElement('a')
+let clip_font_facebook = document.createElement('i')
+let clip_font_whatsapp = document.createElement('i')
+let clip_font_twitter = document.createElement('i')
+let clipText = " \n  Amazingly shortened with trimly. Visit http://trimly.tk to trim your Links!!!"
 
 /**Gets the new trim returned from the server and adds it to the display.
  * Prints an error if the server returns an error message.
  * @param {Response} response. The response object.
- */
-const printNewTrim = (response)=> {
-	if (!response.ok) 
-		showError(response, true);
+ */	
+	const printNewTrim = async(response)=> {
+		if (!response.ok) 
+			showError(response, true);
 
 	// Logic to add new trim to the list here.
-	console.log(response);
+		try{
+			const newClip = await response.json()
+			const {click_count, long_url, urlCode, clipped_url, expiry_date} = await newClip.payload
+			td_click_count.innerText = click_count
+			tr_clip.appendChild(td_click_count)
+			td_long_url.innerText = long_url
+			tr_clip.appendChild(td_long_url)
+
+			a_trimmed.className= "trimmed"
+			a_trimmed.target = "_blank"
+			a_trimmed.href = `/${urlCode}`
+			a_trimmed.innerText = clipped_url
+			td_trimmed.appendChild(a_trimmed)
+			tr_clip.appendChild(td_trimmed)
+
+			td_expiry.id="col-expiry"
+			td_expiry.innerText = expiry_date || '—'
+			tr_clip.appendChild(td_expiry)
+
+
+			a_action_btn.href = "javascript:void(0);"
+			a_action_btn.className= "fas fa-copy fa-lg copy"
+			a_action_btn['data'] = clipped_url
+			a_action_btn['data-tippy-placement'] = "top"
+			a_action_btn['data-tippy-content'] = "COPIED!"
+			a_action_btn.innerText = clipped_url
+			td_action_btn.appendChild(a_action_btn)
+			
+			a_whatsapp.href = `https://api.whatsapp.com/send?&text=${clipped_url} ${clipText}*`
+			clip_font_whatsapp.className= "fab fa-whatsapp fa-lg"
+			a_whatsapp.appendChild(clip_font_whatsapp)
+			td_action_btn.appendChild(a_whatsapp)
+
+			a_facebook.href = `https://www.facebook.com/sharer/sharer.php?u=${clipped_url} ${clipText}&amp;src=sdkpreparse`
+			a_facebook.target="_blank"
+			clip_font_facebook.className= "fab fa-facebook fa-lg"
+			a_facebook.className="fb-xfbml-parse-ignore"
+			a_facebook.appendChild(clip_font_facebook)
+			td_action_btn.appendChild(a_facebook)
+
+			a_twitter.href = `https://twitter.com/intent/tweet?text=${clipped_url} ${clipText}`
+			a_twitter['data-size']="large"
+			clip_font_twitter.className= "fab fa-twitter fa-lg"
+			a_twitter.appendChild(clip_font_twitter)
+			td_action_btn.appendChild(a_twitter)
+
+			tr_clip.appendChild(td_action_btn)
+
+			return tr_body.prepend(tr_clip)
+																
+
+		}catch(error){
+			console.log(error)
+		}
 }
 
 /**Prints an error message for the user.
@@ -29,11 +99,25 @@ const showError = (reason, isServerResponse = false)=> {
 }
 
 trimUrlForm.onsubmit = (e)=> {
-	const urlData = new FormData(trimUrlForm);
+	e.preventDefault()
+	const clipData = new FormData(trimUrlForm);
+	const newObj = {}; // constructing new obj.
+
+	// add the form key/value pairs
+	for (var pair of clipData.entries()) {
+		newObj[pair[0]] = pair[1];
+	}
+	const {created_by, long_url} = newObj
+	const urlData = {
+		created_by, long_url
+	}
 
 	fetch('/', {
 		method: 'POST',
-		body: urlData
+		headers: {
+      'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(urlData)
 	})
 	.then(printNewTrim) //Be sure to handle error response from the server.
 	.catch(showError) //If the browser fails to communicate with the server, handle such errors here.
