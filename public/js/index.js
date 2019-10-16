6e818e6cd575f95a684c1356518f1ffab1951246
Akinlade Feranmi <feranmiakinlade@gmail.com>
2019-10-16 13:38:37 +0100

    Fixed text on copy icon and printNewTrim overwriting previous trims.

--- public/js/index.js
+++ public/js/index.js
@@ -2,88 +2,92 @@ const trimUrlForm = document.querySelector('.trim-url-form');
 const tr_body = document.querySelector('#tbody');
 const clipsListContainer = document.querySelector('#clips-list-container');
 const err_msg = document.querySelector('#msg')
-
-let tr_clip = document.createElement('tr')
-let td_click_count = document.createElement('td')
-let td_long_url = document.createElement('td')
-let td_trimmed = document.createElement('td')
-let td_expiry = document.createElement('td')
-let td_action_btn = document.createElement('td')
-let a_trimmed = document.createElement('a')
-let a_action_btn = document.createElement('a')
-let a_whatsapp = document.createElement('a')
-let a_facebook = document.createElement('a')
-let a_twitter = document.createElement('a')
-let clip_font_facebook = document.createElement('i')
-let clip_font_whatsapp = document.createElement('i')
-let clip_font_twitter = document.createElement('i')
-let clipText = " \n  Amazingly shortened with trimly. Visit http://trimly.tk to trim your Links!!!";
+const clipText = " \n  Amazingly shortened with trimly. Visit http://trimly.tk to trim your Links!!!";
 
 /**Gets the new trim returned from the server and adds it to the display.
  * Prints an error if the server returns an error message.
  * @param {Response} response. The response object.
  */	
 const printNewTrim = async(response)=> {
-		if (!response.ok) 
-			showError(response, true);
+	if (!response.ok) 
+		return showError(response, true);
+
+	let tr_clip = document.createElement('tr')
+	let td_click_count = document.createElement('td')
+	let td_long_url = document.createElement('td')
+	let td_trimmed = document.createElement('td')
+	let td_expiry = document.createElement('td')
+	let td_action_btn = document.createElement('td')
+	let a_trimmed = document.createElement('a')
+	let a_action_btn = document.createElement('a')
+	let a_whatsapp = document.createElement('a')
+	let a_facebook = document.createElement('a')
+	let a_twitter = document.createElement('a')
+	let clip_font_facebook = document.createElement('i')
+	let clip_font_whatsapp = document.createElement('i')
+	let clip_font_twitter = document.createElement('i')
 
 	// Logic to add new trim to the list here.
-		try{
-			const newClip = await response.json()
-			const {click_count, long_url, urlCode, clipped_url, expiry_date} = await newClip.payload
-			td_click_count.innerText = click_count
-			tr_clip.appendChild(td_click_count)
-			td_long_url.innerText = long_url
-			tr_clip.appendChild(td_long_url)
+	try {
+		const newClip = await response.json()
+		const {click_count, long_url, urlCode, clipped_url, expiry_date} = await newClip.payload;
 
-			a_trimmed.className= "trimmed"
-			a_trimmed.target = "_blank"
-			a_trimmed.href = `/${urlCode}`
-			a_trimmed.innerText = clipped_url
-			td_trimmed.appendChild(a_trimmed)
-			tr_clip.appendChild(td_trimmed)
+		td_action_btn.classList.add('action-btn');
 
-			td_expiry.id="col-expiry"
-			td_expiry.innerText = expiry_date || '—'
-			tr_clip.appendChild(td_expiry)
+		td_click_count.innerText = click_count
+		tr_clip.appendChild(td_click_count)
+		td_long_url.innerText = long_url
+		tr_clip.appendChild(td_long_url)
+
+		a_trimmed.className= "trimmed"
+		a_trimmed.target = "_blank"
+		a_trimmed.href = `/${urlCode}`
+		a_trimmed.innerText = clipped_url
+		td_trimmed.appendChild(a_trimmed)
+		tr_clip.appendChild(td_trimmed)
+
+		td_expiry.id="col-expiry"
+		td_expiry.innerText = expiry_date || '—'
+		tr_clip.appendChild(td_expiry)
 
 
-			a_action_btn.href = "javascript:void(0);"
-			a_action_btn.className= "fas fa-copy fa-lg copy"
-			a_action_btn['data'] = clipped_url
-			a_action_btn['data-tippy-placement'] = "top"
-			a_action_btn['data-tippy-content'] = "COPIED!"
-			a_action_btn.innerText = clipped_url
-			td_action_btn.appendChild(a_action_btn)
+		a_action_btn.href = "javascript:void(0);"
+		a_action_btn.className= "fas fa-copy fa-lg copy"
+		a_action_btn['data'] = clipped_url;
+		a_action_btn['data-tippy-placement'] = "top"
+		a_action_btn['data-tippy-content'] = "COPIED!"
+		td_action_btn.appendChild(a_action_btn);
+		
+		a_whatsapp.href = `https://api.whatsapp.com/send?&text=${clipped_url} ${clipText}*`
+		clip_font_whatsapp.className= "fab fa-whatsapp fa-lg"
+		a_whatsapp.appendChild(clip_font_whatsapp)
+		td_action_btn.appendChild(a_whatsapp)
+
+		a_facebook.href = `https://www.facebook.com/sharer/sharer.php?u=${clipped_url} ${clipText}&amp;src=sdkpreparse`
+		a_facebook.target="_blank"
+		clip_font_facebook.className= "fab fa-facebook fa-lg"
+		a_facebook.className="fb-xfbml-parse-ignore"
+		a_facebook.appendChild(clip_font_facebook)
+		td_action_btn.appendChild(a_facebook)
+
+		a_twitter.href = `https://twitter.com/intent/tweet?text=${clipped_url} ${clipText}`
+		a_twitter['data-size']="large"
+		clip_font_twitter.className= "fab fa-twitter fa-lg"
+		a_twitter.appendChild(clip_font_twitter)
+		td_action_btn.appendChild(a_twitter)
+
+		tr_clip.appendChild(td_action_btn)
+
+		clipsListContainer.style.display = "initial";
+		tr_body.prepend(tr_clip)											
+	}									
+		//Handle browser error here.
+	catch(error) {
+		if(error.message === 'Body has already been consumed.')
+			return;
 			
-			a_whatsapp.href = `https://api.whatsapp.com/send?&text=${clipped_url} ${clipText}*`
-			clip_font_whatsapp.className= "fab fa-whatsapp fa-lg"
-			a_whatsapp.appendChild(clip_font_whatsapp)
-			td_action_btn.appendChild(a_whatsapp)
-
-			a_facebook.href = `https://www.facebook.com/sharer/sharer.php?u=${clipped_url} ${clipText}&amp;src=sdkpreparse`
-			a_facebook.target="_blank"
-			clip_font_facebook.className= "fab fa-facebook fa-lg"
-			a_facebook.className="fb-xfbml-parse-ignore"
-			a_facebook.appendChild(clip_font_facebook)
-			td_action_btn.appendChild(a_facebook)
-
-			a_twitter.href = `https://twitter.com/intent/tweet?text=${clipped_url} ${clipText}`
-			a_twitter['data-size']="large"
-			clip_font_twitter.className= "fab fa-twitter fa-lg"
-			a_twitter.appendChild(clip_font_twitter)
-			td_action_btn.appendChild(a_twitter)
-
-			tr_clip.appendChild(td_action_btn)
-
-			clipsListContainer.style.display = "initial";
-			tr_body.prepend(tr_clip)											
-		}									
-			//Handle browser error here.
-		catch(error) {
-			if(error.message === 'Body has already been consumed.')
-				return
-		}
+		console.log(error);
+	}
 }
 
 /**Prints an error message for the user.
