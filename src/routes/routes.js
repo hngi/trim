0ae2e258dfa9739fcf1cd9a79b60c4ecfc1b590a
Oluwaseyi Ogunjuyigbe <seyiogunjuyigbe@gmail.com>
2020-04-03 12:10:15 +0100

    docs redirected to postman docs

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -20,10 +20,10 @@ import { getUrlClickMetrics } from '../controllers/metricsController';
 export const initRoutes = app => {
   app.get("/", validateCookie, renderLandingPage);
   app.get("/about", (req, res) => res.status(200).render("about"));
-  app.post("/", [check('long_url').isString().not().isEmpty().withMessage('Long url cannot be empty'),
+  app.get("/docs", (req,res)=>res.status(200).redirect("https://documenter.getpostman.com/view/4447136/SzYaWe6j?version=latest"));
+   app.post("/", [check('long_url').isString().not().isEmpty().withMessage('Long url cannot be empty'),
       check('expiry_date').optional().matches(/([12]\d{3}[-\/](0[1-9]|1[0-2])[-\/](0[1-9]|[12]\d|3[01]))/).withMessage('Date format has to be yyyy-mm-dd or yyyy/mm/dd')]
             ,validate, validateOwnDomain, stripUrl, urlAlreadyTrimmedByUser, customUrlExists, trimUrl);
-  app.get("/about", aboutPage);
   app.get("/:id", getUrlAndUpdateCount);
   app.get('/metrics/:urlShortenId', getUrlClickMetrics);
   app.all("*", (req, res) => res.status(404).render("error"));
